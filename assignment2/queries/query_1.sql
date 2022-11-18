13450884faec8b36268bcbef1017aba55fcc7111
Congiuntivo <simone.decarli@studenti.unitn.it>
2022-11-18 13:43:53 +0100

    Assignment2 second query

--- assignment2/queries/query_1.sql
+++ assignment2/queries/query_1.sql
@@ -3,7 +3,6 @@ SELECT
     COALESCE(MAX(movies.gross - movies.budget), -1) AS max,
     COALESCE(TRUNC(AVG(movies.gross - movies.budget), 2), -1.00) AS avg
 FROM directors LEFT JOIN movies ON directors.director = movies.director
-WHERE directors.yearofbirth > 1972
-GROUP BY directors.director;
-
-SELECT CAST(1 AS DECIMAL(100,2));
+WHERE EXTRACT(YEAR FROM CURRENT_DATE) -  directors.yearofbirth > 50
+GROUP BY directors.director
+ORDER BY min, max, avg;
