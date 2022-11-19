1baf2428f058c2e2400e3699dbb624b0f9a4463d
Congiuntivo <simone.decarli@studenti.unitn.it>
2022-11-19 18:36:26 +0100

    assignment 2, queries 3 and 4

--- assignment2/queries/query_1.sql
+++ assignment2/queries/query_1.sql
@@ -1,7 +1,7 @@
 SELECT
-    COALESCE(MIN(movies.gross - movies.budget), -1) AS min,
-    COALESCE(MAX(movies.gross - movies.budget), -1) AS max,
-    COALESCE(TRUNC(AVG(movies.gross - movies.budget), 2), -1.00) AS avg
+    COALESCE(MIN(movies.gross - movies.budget), -1) AS minimum,
+    COALESCE(MAX(movies.gross - movies.budget), -1) AS maximum,
+    COALESCE(TRUNC(AVG(movies.gross - movies.budget), 2), -1) AS average
 FROM directors LEFT JOIN movies ON directors.director = movies.director
 WHERE EXTRACT(YEAR FROM CURRENT_DATE) -  directors.yearofbirth > 50
 GROUP BY directors.director
---
+++ assignment2/queries/query_3.sql
@@ -0,0 +1,8 @@
+(SELECT DISTINCT 'most profitable' AS feature, title, year
+FROM movies
+WHERE  movies.gross - movies.budget = (SELECT MAX(movies.gross - movies.budget) FROM movies)
+UNION ALL
+SELECT DISTINCT 'least expensive' AS feature, title, year
+FROM movies
+WHERE  movies.budget = (SELECT MIN(movies.budget) FROM movies))
+ORDER BY feature, title, year
