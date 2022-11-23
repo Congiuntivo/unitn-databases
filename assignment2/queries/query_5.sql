8f4262f3aef07eb573ed9963184522b8aed86f3b
Congiuntivo <simone.decarli@studenti.unitn.it>
2022-11-23 11:53:24 +0100

    fixed query 5

--- assignment2/queries/query_5.sql
+++ assignment2/queries/query_5.sql
@@ -7,8 +7,14 @@ WITH
         (
             SELECT title, year, COUNT(movieawards.title) as won FROM movieawards WHERE movieawards.result = 'won' GROUP BY title, year
         )
-SELECT movies.title, movies.year, COALESCE(ROUND(ROUND(COALESCE(won, 0),2)/nominated,2), -1) AS succes_rate
+SELECT movies.title, movies.year,
+       CASE
+        WHEN won IS NOT NULL AND nominated IS NOT NULL THEN ROUND(ROUND(won,2)/(ROUND(won,2)+ROUND(nominated,2)),2)
+        WHEN won IS NULL AND nominated IS NULL THEN -1
+        WHEN won IS NULL AND nominated IS NOT NULL THEN 0.00
+        WHEN won IS NOT NULL AND nominated IS NULL THEN 1.00
+        END AS success_rate
 FROM movies
     LEFT JOIN nomine ON movies.title = nomine.title AND movies.year = nomine.year
     LEFT JOIN vincite ON movies.title = vincite.title AND movies.year = vincite.year
-ORDER BY movies.title, movies.year, succes_rate;
+ORDER BY movies.title, movies.year, success_rate;
