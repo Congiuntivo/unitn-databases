3c7c0962680b708dbce30c1b1c1059e3fcaf4ab6
Congiuntivo <simone.decarli@studenti.unitn.it>
2022-11-28 13:02:56 +0100

    w gesoo e la query 10

--- assignment2/queries/query_10.sql
+++ assignment2/queries/query_10.sql
@@ -5,8 +5,24 @@ WITH
             FROM movieawards INNER JOIN movies ON movieawards.title = movieS.title AND movieawards.year = movies.year
             WHERE MOVIES.director = 'Spielberg' AND movieawards.result = 'won'
             GROUP BY movies.title, movies.year
+        ),
+    best_dir AS
+        (
+            SELECT director, year
+            FROM directorawards
+            WHERE result = 'won'
+            UNION DISTINCT
+            SELECT director, movies.year
+            FROM movies INNER JOIN movieawards ON movies.title = movieawards.title AND movies.year = movieawards.year
+            WHERE award LIKE '%best director' AND result = 'won'
+        ),
+    to_avoid AS
+        (
+            SELECT *
+            FROM (SELECT director FROM best_dir) AS dir CROSS JOIN (SELECT year FROM film_spi WHERE num >= 3) AS anni
+            EXCEPT
+            (SELECT * FROM best_dir)
         )
-SELECT DISTINCT director
-FROM movies INNER JOIN movieawards ON movies.title = movieawards.title AND movies.year = movieawards.year
-WHERE movieawards.award LIKE '%best director' AND movieawards.result = 'won' AND movies.year IN (SELECT film_spi.year FROM film_spi WHERE num >=3)
-ORDER BY director;
+SELECT director
+FROM best_dir
+WHERE director NOT IN (SELECT director FROM to_avoid)
