1eb9223d4c12878eecd02b87e697bb87f2bd0059
Sebastiano Tocci <seba.tocci@gmail.com>
2022-11-28 23:44:31 +0100

    agiuornamento nebro

--- assignment2/queries_seba/query_8.sql
+++ assignment2/queries_seba/query_8.sql
@@ -3,10 +3,11 @@ SELECT (CASE
                 THEN ROUND(won / tot, 2)
             ELSE -1 END) as percentage
 FROM (SELECT CAST(COUNT(*) as DECIMAL) as won
-      FROM movieawards
-      WHERE year BETWEEN 1980 AND 1989
-        AND result = 'won'
-        AND award LIKE '%Oscar%') as tmp1,
+      FROM (SELECT DISTINCT title, year
+            FROM movieawards
+            WHERE year BETWEEN 1980 AND 1989
+              AND result = 'won'
+              AND award LIKE '%Oscar%') as tmp3) as tmp1,
      (SELECT CAST(COUNT(*) as DECIMAL) as tot
       FROM movies
       WHERE year BETWEEN 1980 AND 1989) as temp2;
