c14e49bda18a92390e207c6e664c4ad3ab4e1ff1
lorys <lorys.mutti@studenti.unitn.it>
2022-11-30 14:18:26 +0100

    query 5

--- assignment2/queries_lorys/query_5.sql
+++ assignment2/queries_lorys/query_5.sql
@@ -18,7 +18,7 @@ WITH won_awards AS
           WHERE award ISNULL
           GROUP BY m.title, m.year)
 
-SELECT w2.title, w2.year, COALESCE(round((c1 / c2::numeric), 2),-1) as success_rate
+SELECT w2.title, w2.year, COALESCE(round((c1 / c2::numeric), 2),0) as success_rate
 FROM won_awards w1
          RIGHT JOIN all_awards w2 ON w2.title = w1.title AND w2.year = w1.year
 UNION ALL
