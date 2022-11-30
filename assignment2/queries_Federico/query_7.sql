ff59a283bdf3f3872999534e3768ff184f001fa6
Federico Frigerio <federico.frigerio@studenti.unitn.it>
2022-11-30 10:02:03 +0100

    Corretta query 7

--- assignment2/queries_Federico/query_7.sql
+++ assignment2/queries_Federico/query_7.sql
@@ -1,21 +1,22 @@
-WITH d AS (SELECT d3.director, yearofbirth
-           FROM directorawards AS aw
-                    JOIN directors d3 ON d3.director = aw.director
-           WHERE award LIKE '%Oscar%'
-             AND result = 'won'
-           UNION
-           SELECT d2.director, yearofbirth
-           FROM movieawards AS aw
-                    JOIN movies m ON aw.title = m.title AND aw.year = m.year
-                    JOIN directors d2 ON m.director = d2.director
-           WHERE award LIKE '%best director%'
-             AND result = 'won'
-           ORDER BY yearofbirth)
-SELECT DISTINCT director, 'oldest' AS feature
-FROM d
-WHERE yearofbirth = (SELECT MIN(yearofbirth) FROM d)
+WITH oscars AS
+         (SELECT d.director, d.yearofbirth
+          FROM directors AS d
+                   INNER JOIN directorawards AS aw ON d.director = aw.director
+          WHERE aw.result = 'won'
+            AND aw.award LIKE '%Oscar%'
+          UNION
+          DISTINCT
+          SELECT directors.director, directors.yearofbirth
+          FROM directors
+                   INNER JOIN movies ON movies.director = directors.director
+                   INNER JOIN movieawards AS mw ON mw.title = movies.title AND movies.year = mw.year
+          WHERE mw.result = 'won'
+            AND mw.award LIKE '%Oscar%best director%')
+SELECT DISTINCT oscars.director, 'oldest' AS feature
+FROM oscars
+WHERE oscars.yearofbirth = (SELECT MIN(yearofbirth) FROM oscars)
 UNION
-SELECT DISTINCT director, 'youngest'
-FROM d
-WHERE yearofbirth = (SELECT MAX(yearofbirth) FROM d)
-ORDER BY director;
+SELECT DISTINCT oscars.director, 'youngest' AS feature
+FROM oscars
+WHERE oscars.yearofbirth = (SELECT MAX(yearofbirth) FROM oscars)
+ORDER BY director, feature;
