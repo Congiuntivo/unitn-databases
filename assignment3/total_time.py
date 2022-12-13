42f5bf9fdaa1e6234d9429b2759f0a68484c1edc
Congiuntivo <simone.decarli@studenti.unitn.it>
2022-12-13 11:53:10 +0100

    I'm so dumb that I cannot evaluate seconds -_-

--- assignment3/total_time.py
+++ assignment3/total_time.py
@@ -3,7 +3,7 @@ total_time = 0
 for line in input:
     total_time += int(line.split()[3])
 minutes = total_time // (60 * 1000 * 1000)
-seconds = total_time % (60 * 1000 * 1000) * 0.01 * 60
-seconds = int(str(seconds)[0:2])
+seconds = (total_time // (1000 * 1000)) % 60
+print("Total time: %d" % total_time)
 print("Total time: %d:%d" % (minutes, seconds))
 input.close()
