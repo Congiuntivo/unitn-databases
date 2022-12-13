a6a0ae60c351e9bd3d1d03a80b5b548e013b9fcf
Congiuntivo <simone.decarli@studenti.unitn.it>
2022-12-13 16:38:40 +0100

    removed time in "ns" print

--- assignment3/total_time.py
+++ assignment3/total_time.py
@@ -4,6 +4,5 @@ for line in input:
     total_time += int(line.split()[3])
 minutes = total_time // (60 * 1000 * 1000)
 seconds = (total_time // (1000 * 1000)) % 60
-print("Total time: %d" % total_time)
 print("Total time: %d:%d" % (minutes, seconds))
 input.close()
