81410ae499b3cb77268294392430c2baa0947258
Congiuntivo <simone.decarli@studenti.unitn.it>
2022-12-14 12:26:54 +0100

    so dumb that I thought nano was 10^-6 -_-

--- assignment3/total_time.py
+++ assignment3/total_time.py
@@ -1,8 +1,9 @@
+NANO_TO_SEC = 1000 * 1000 * 1000
 input = open("A3_227545.txt")
 total_time = 0
 for line in input:
     total_time += int(line.split()[3])
-minutes = total_time // (60 * 1000 * 1000)
-seconds = (total_time // (1000 * 1000)) % 60
+minutes = total_time // (60 * NANO_TO_SEC)
+seconds = (total_time // NANO_TO_SEC) % 60
 print("Total time: %d:%d" % (minutes, seconds))
 input.close()
